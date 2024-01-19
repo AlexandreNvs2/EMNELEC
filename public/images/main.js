a9fec5f8ac041418e3de9f8957f7e398d99614fe
Alexandre <alexandre.neves0402@gmail.com>
2024-01-20 00:27:02 +0100

    Navbar disparu + navbar fixed

--- public/images/main.js
+++ public/images/main.js
@@ -1,3 +1,4 @@
+/*Barre animé */
 document.addEventListener('scroll', function() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -10,3 +11,4 @@ function updateProgressBar(percentage) {
     const progressBar = document.getElementById('progress-bar');
     progressBar.style.width = percentage + '%';
 }
+
